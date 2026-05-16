aa1805b13345e677a5cf2c8cbefca0ae301165e4
Govardhan <govardhangopu0@gmail.com>
2026-05-16 19:55:33 +0530

    env vars for railway added

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,10 +8,11 @@ let pool;
 export const connectDB = () => {
     if (!pool) {
         pool = mysql.createPool({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME,
+            host: process.env.MYSQLHOST || process.env.DB_HOST,
+            user: process.env.MYSQLUSER || process.env.DB_USER,
+            password: process.env.MYSQLPASSWORD || process.env.DB_PASS,
+            database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+            port: Number(process.env.MYSQLPORT) || (process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306),
             waitForConnections: true,
             connectionLimit: 10,
         });
